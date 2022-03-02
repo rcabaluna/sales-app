59de0cc2d9f54c496fb915102dd8576b47d91b07
rcabaluna <rcabalunajr@gmail.com>
2022-03-02 17:29:26 +0800

    Update sales-app.py

--- sales-app.py
+++ sales-app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.externals import joblib
-import seaborn as sns
+import joblib
+import seaborn as sns 
 
 # Title
 st.title("Sales Calculator App")
